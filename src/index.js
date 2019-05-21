7cc812990f781b54d73b7c486acb273b4f605170
phani2585 <phani2585@gmail.com>
2019-05-21 18:33:41 +0530

    Commit latest code including Header for Login Page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from '../src/screens/login/Login';
 
-ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
-);
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
