774003aef0cd49f31c1cc54fb8bdad2e75476a2b
phani2585 <>
2019-05-30 05:58:05 +0530

    Latest code save commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import Controller from '../src/common/Controller';
-import Profile from '../src/screens/profile/Profile';
+import Controller from '../src/common/Controller';
+//import Profile from '../src/screens/profile/Profile';
 //import Home from '../src/screens/home/Home';
 import 'typeface-roboto';
 
@@ -11,7 +11,7 @@ import 'typeface-roboto';
 //ReactDOM.render(<Home/> , document.getElementById('root'));
 
 
-//ReactDOM.render(<Controller/>, document.getElementById('root'));
+ReactDOM.render(<Controller/>, document.getElementById('root'));
 
 /* Temporarily used to view content of Profile page till routing to Profile Page developed*/
-ReactDOM.render(<Profile/>, document.getElementById('root'));
+//ReactDOM.render(<Profile/>, document.getElementById('root'));
