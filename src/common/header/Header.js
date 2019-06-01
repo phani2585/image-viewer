0dc39c518fda58acf8594ce54a22ca07cf83723e
phani2585 <>
2019-06-02 02:06:55 +0530

    Header css set right

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,28 +1,113 @@
-import React,{Component} from 'react';
-//import AppSearchBar from '../AppSearchBar';
-import './Header.css'; 
+import React, { Component } from 'react';
+import './Header.css';
+import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
-/* Header component class is defined in this file.This file is COMMON for all the pages */
+
+const styles = theme => ({
+    grow: {
+        flexGrow: 1,
+    },
+    title: {
+        color: 'initial',
+    },
+    bigAvatar: {
+        margin: 10,
+        width: 60,
+        height: 60,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: '4px',
+        backgroundColor: '#c0c0c0',
+        marginLeft: '70%',
+        width: '300px',
+        verticalAlign: 'center',
+    },
+    searchIcon: {
+        height: '100%',
+        color: 'black',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
+
+});
 
 class Header extends Component {
 
     constructor() {
         super();
         this.state = {
-            loggedIn: sessionStorage.getItem("access-token") == null ? false : true
+            ownerInfo: [],
         }
+        this.baseUrl = "https://api.instagram.com/v1/users/self/?access_token=13521022383.d5e23ae.c9785a17269b494eb996c2cbc490a6f3";
+    }
+
+//Accessing data from backend API 1
+    componentWillMount() {
+
+        // Get owner info after authenticating the  accessToken generated 
+        let ownerData = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    ownerInfo: JSON.parse(this.responseText).data
+
+                });
+            }
+        })
+        xhr.open("GET", this.baseUrl);
+        xhr.send(ownerData);
     }
 
     render() {
+
+        const { classes } = this.props;
+
         return (
-            <div>
-                <header className="app-header app-logo">
-                  Image Viewer
-                                        
-                </header>
+            <div className={classes.grow}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography className={{ color: "app-logo" }} variant="h6" noWrap>Image Viewer</Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="        Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                        {<Avatar className="avatar">
+                            <img aria-controls="simpleMenu" onClick={this.openMenuHandler} src={this.state.ownerInfo.profile_picture} alt={"logo"} /></Avatar>}
+                    </Toolbar>
+                </AppBar>
             </div>
-        )
+        );
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
